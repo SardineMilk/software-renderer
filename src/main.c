b76219b7e96fdedd42f83821b54add28b34233e0
SardineMilk <raspberryhaggises@gmail.com>
2025-11-12 17:15:04 +0000

    Define macros for width and height

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+// Size of window
+#define WIDTH 800  
+#define HEIGHT 600
+
+
 
 typedef struct {
     int r;
@@ -9,14 +14,21 @@ typedef struct {
 } Color;
 
 typedef struct {
-    int width;
-    int height;
-} Uniforms;
+    int mask;
+} Chunk;
 
-static Color shader(int x, int y, const Uniforms *uni) {
-    int r = (float)x / ((float)uni->width/255.0);
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vector3;
+
+
+
+static Color shader(int x, int y) {
+    int r = (x*255) / WIDTH;
     int g = 0;
-    int b = (float)y / ((float)uni->height/255.0);
+    int b = (y*255) / HEIGHT;
 
     Color color = {r, g, b};
     return color;
@@ -29,12 +41,10 @@ int main() {
         return 1;
     }
 
-    int width = 800;
-    int height = 600;
     SDL_Window *win = SDL_CreateWindow(
         "SDL2 Software Renderer", 
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        width, height, SDL_WINDOW_SHOWN
+        WIDTH, HEIGHT, SDL_WINDOW_SHOWN
     );
 
     if (!win) {
@@ -51,7 +61,6 @@ int main() {
         return 1;
     }
 
-    Uniforms uni = {width, height};
 
     // Main Loop
     int running = 1;
@@ -68,9 +77,9 @@ int main() {
         int pitch = surf->pitch / 4;  // Number of bytes between the start of each row in memory
 
         // Loop over every pixel
-        for (int y = 0; y < height; ++y) {
-            for (int x = 0; x < width; ++x) {
-                Color pixel_color = shader(x, y, &uni);
+        for (int y = 0; y < HEIGHT; ++y) {
+            for (int x = 0; x < WIDTH; ++x) {
+                Color pixel_color = shader(x, y);
                 pixels[(y * pitch) + x] = 0xFF000000 | (pixel_color.r << 16) | (pixel_color.g << 8) | pixel_color.b;
             }
         }
