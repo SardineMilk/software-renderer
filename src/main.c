346c1b0cbdddafd0aefe02d643b926a3c6b9b56a
SardineMilk <raspberryhaggises@gmail.com>
2025-11-15 16:36:58 +0000

    Add multithreading

--- src/main.c
+++ src/main.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <pthread.h>
+
+#include "vector.h"
 
 // Size of window
-#define WINDOW_WIDTH 800 
-#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 720
+#define WINDOW_HEIGHT 720
 
-#define RENDER_WIDTH 128 
-#define RENDER_HEIGHT 128
+#define RENDER_WIDTH 180 
+#define RENDER_HEIGHT 180
 
 // Hardcoded delay between frames
 #define FRAME_DELAY 16
@@ -32,11 +35,6 @@ typedef struct {
     uint64_t ChildMask;
 } Chunk;
 
-typedef struct {
-    float x;
-    float y;
-    float z;
-} Vec3;
 
 typedef struct {
     float m[3][3];
@@ -53,25 +51,14 @@ typedef struct {
 } Camera;
 
 
-static inline Vec3 add(Vec3 a, Vec3 b) {
-    return (Vec3){a.x + b.x, a.y + b.y, a.z + b.z};
-}
-
-static inline Vec3 sub(Vec3 a, Vec3 b) {
-    return (Vec3){a.x - b.x, a.y - b.y, a.z - b.z};
-}
-
-static inline Vec3 scale(Vec3 v, float s) {
-    return (Vec3){v.x * s, v.y * s, v.z * s};
-}
-
-static inline float length(Vec3 v) {
-    return sqrtf(v.x*v.x + v.y*v.y + v.z*v.z);
-}
-
-static inline float sqlength(Vec3 v) {
-    return v.x*v.x + v.y*v.y + v.z*v.z;
-}
+typedef struct {
+    int y_start;
+    int y_end;
+    Camera *camera;
+    Vec3 forward, right, up;
+    uint32_t *pixels;
+    int pitch;
+} RenderJob;
 
 static Mat3 mat3_mul(Mat3 a, Mat3 b) {
     Mat3 r;
@@ -96,35 +83,49 @@ Vec3 normalize(Vec3 v) {
 }
 
 static HitInfo raycast(Vec3 ro, Vec3 rd) {
-    int marching_steps = 100;
-    int max_distance = 10;
-
     Vec3 ray_position = ro;
-    float distance;
     bool hit = false;
-    for (int i = 0; i < marching_steps; ++i) {
-        distance = length(ray_position) - 0.5;
+    Vec3 intersection_position = ray_position;
+    Vec3 inv_rd = (Vec3){1.0f/rd.x, 1.0f/rd.y, 1.0f/rd.z};
 
-        if (distance < 0.005) {
-            hit = true;
-            break;
-        } 
+    // Cube bounds
+    const float xmin = 0.0f, ymin = 0.0f, zmin = 0.0f;
+    const float xmax = 1.0f, ymax = 1.0f, zmax = 1.0f;
 
-        if (distance > max_distance) {
-            break;
-        }
+    // Compute intersection using slab method
+    float t1 = (xmin - ro.x) * inv_rd.x;
+    float t2 = (xmax - ro.x) * inv_rd.x;
+    float tmin = fminf(t1, t2);
+    float tmax = fmaxf(t1, t2);
 
-        ray_position = add(ray_position, scale(rd, distance));
+    float ty1 = (ymin - ro.y) * inv_rd.y;
+    float ty2 = (ymax - ro.y) * inv_rd.y;
+    tmin = fmaxf(tmin, fminf(ty1, ty2));
+    tmax = fminf(tmax, fmaxf(ty1, ty2));
+
+    float tz1 = (zmin - ro.z) * inv_rd.z;
+    float tz2 = (zmax - ro.z) * inv_rd.z;
+    tmin = fmaxf(tmin, fminf(tz1, tz2));
+    tmax = fminf(tmax, fmaxf(tz1, tz2));
+
+    if (tmax >= tmin && tmax >= 0.0f) {
+        hit = true;
+        float t = (tmin >= 0.0f) ? tmin : tmax;  // pick nearest positive
+        intersection_position = (Vec3){
+            ro.x + rd.x * t,
+            ro.y + rd.y * t,
+            ro.z + rd.z * t
+        };
     }
 
-    return (HitInfo){ray_position, hit};
+    return (HitInfo){intersection_position, hit};
 }
 
 static Color shader(int x, int y, Camera *camera, Vec3 forward, Vec3 right, Vec3 up) {
     float u = (2.0f * x / RENDER_WIDTH - 1.0f);
     float v = (2.0f * y / RENDER_HEIGHT - 1.0f);
 
-
+    // direction is forward + a small right/up bias depending on the pixel position
     Vec3 ray_direction = add(forward, add(scale(right, u), scale(up, v)));
     ray_direction = normalize(ray_direction);
 
@@ -134,8 +135,8 @@ static Color shader(int x, int y, Camera *camera, Vec3 forward, Vec3 right, Vec3
 
 
     if (ray_hit.hit) {
-        float shade = length(ray_hit.position);
-        Color color = {255*shade, 0, 255*shade};
+        float shade = length(scale(sub(ray_hit.position, (Vec3){0.5, 0.5, 0.5}), 0.5f));
+        Color color = {127-(255*shade), 0, 255*shade};
         return color;
     }
     else {
@@ -160,6 +161,21 @@ static Mat3 camera_matrix(Camera *cam) {
     }};
 
     return mat3_mul(Ry, Rx);
+}
+
+void* render_thread(void *arg) {
+    // Render a chunk of pixels
+    RenderJob *job = (RenderJob*)arg;
+
+    for (int y = job->y_start; y < job->y_end; ++y) {
+        for (int x = 0; x < RENDER_WIDTH; ++x) {
+            Color c = shader(x, y, job->camera, job->forward, job->right, job->up);
+            job->pixels[y * job->pitch + x] =
+                0xFF000000 | (c.r << 16) | (c.g << 8) | c.b;
+        }
+    }
+
+    return NULL;
 }
 
 int main() {
@@ -199,9 +215,15 @@ int main() {
         return 1;
     }
 
-    Vec3 position = {0, 0, 0};
-    Vec3 rotation = {0, 0, 0};
-    Camera camera = {position, rotation};
+    // Camera
+    Camera camera = {(Vec3){0, 0, 0}, (Vec3){0, 0, 0}};
+
+    // Multithreading variables
+    int thread_count = SDL_GetCPUCount();
+    pthread_t threads[16];
+    RenderJob jobs[16];
+
+    int rows_per_thread = RENDER_HEIGHT / thread_count;
 
     // Main Loop
     int running = 1;
@@ -251,6 +273,28 @@ int main() {
         uint32_t *pixels = (uint32_t *)render_surf->pixels;
         int pitch = render_surf->pitch / 4;  // Number of bytes between the start of each row in memory
 
+
+        for (int i = 0; i < thread_count; i++) {
+            int y0 = i * rows_per_thread;  // starting y
+            int y1 = (i == thread_count - 1) ? RENDER_HEIGHT : y0 + rows_per_thread;  // ending y. if last thread, take all the rest
+
+            jobs[i] = (RenderJob){
+                .y_start = y0,
+                .y_end   = y1,
+                .camera  = &camera,
+                .forward = forward,
+                .right   = right,
+                .up      = up,
+                .pixels  = pixels,
+                .pitch   = pitch
+            };
+
+            pthread_create(&threads[i], NULL, render_thread, &jobs[i]);
+        }
+
+        for (int i = 0; i < thread_count; i++)
+            pthread_join(threads[i], NULL);
+        /*
         // Loop over every pixel
         for (int y = 0; y < RENDER_HEIGHT; ++y) {
             for (int x = 0; x < RENDER_WIDTH; ++x) {
@@ -258,16 +302,17 @@ int main() {
                 pixels[(y * pitch) + x] = 0xFF000000 | (pixel_color.r << 16) | (pixel_color.g << 8) | pixel_color.b;
             }
         }
-
+        */
         SDL_BlitScaled(render_surf, NULL, window_surf, NULL);   
         SDL_UpdateWindowSurface(win);
 
         Uint64 end = SDL_GetPerformanceCounter();
         float elapsed = (float)(end - start) / (float)SDL_GetPerformanceFrequency();
-        printf("FPS: %f\n", 1.0f / elapsed);
-
-        //SDL_Delay(FRAME_DELAY-elapsed);
-
+        printf("Uncapped FPS: %f\n", 1.0f / elapsed);
+        
+        float target = FRAME_DELAY / 1000.0f;
+        if (elapsed < target)
+            SDL_Delay((Uint32)((target - elapsed) * 1000.0f));
     }
 
     SDL_DestroyWindow(win);
---
+++ src/vector.h
@@ -0,0 +1,35 @@
+// vector.h
+#ifndef VECTOR_H
+#define VECTOR_H
+
+
+#include <math.h>
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vec3;
+
+
+static inline Vec3 add(Vec3 a, Vec3 b) {
+    return (Vec3){a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+static inline Vec3 sub(Vec3 a, Vec3 b) {
+    return (Vec3){a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+static inline Vec3 scale(Vec3 v, float s) {
+    return (Vec3){v.x * s, v.y * s, v.z * s};
+}
+
+static inline float length(Vec3 v) {
+    return sqrtf(v.x*v.x + v.y*v.y + v.z*v.z);
+}
+
+static inline float sqlength(Vec3 v) {
+    return v.x*v.x + v.y*v.y + v.z*v.z;
+}
+
+#endif // VECTOR_H
